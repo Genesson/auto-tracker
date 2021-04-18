f31786b77b44409ddd47f3dc3af8ba04e1eaca11
Genesson <genesson.sauer@mjv.net>
2021-04-18 15:47:35 -0300

    feat: implement verify project id method

--- background.js
+++ background.js
@@ -17,10 +17,9 @@ chrome.extension.onConnect.addListener((port) => {
 
   port.onMessage.addListener(function(data) {
     this.sendFeedback();
-    console.log(list);
 
     if (list && typeof data === 'object') {
-      formatList(data);
+      loginClockfy(data);
     } else if(feedback.loadedList === false) {
       console.log(data);
     }
@@ -37,60 +36,7 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
   feedback.loadedList = true;
 });
 
-function formatList(user) {
-  const tasksToArray = user.activity.split("\n");
-
-  const dataTask = [];
-
-  tasksToArray.map(item => {
-    const task = {
-      date: item.trim().substring(0, item.trim().indexOf(" ")),
-      description: item.trim().substring(item.trim().indexOf(" ")).trim()
-    }
-    dataTask.push(task);
-  });
-
-  const DESCRIPTION_DEFAULT = user.prefix;
-  const PROJECT_ID = "5ee7d2cf6e009831b01cecb7";
-  const hourIntervalStart = " " + user.start_interval + ":00";
-  const hourIntervalEnd = " " + user.end_interval + ":00";
-
-  formattedList = [];
-
-  list = list.filter(item => item.marcacao1 != null);
-  list = list.slice(0, 3);
-
-  list.map(item => {
-    const descriptionTask = dataTask.filter(task => task.date === item.dt);
-    const description = descriptionTask[0] ? DESCRIPTION_DEFAULT + " | " + (descriptionTask[0].description) : DESCRIPTION_DEFAULT;
-
-    const data = {
-      billable: true,
-      description: description,
-      projectId: PROJECT_ID,
-      taskId: null,
-      tagIds: null
-    }
-
-    const firstPeriod = {
-      ...data,
-      start: new Date(new Date(item.marcacao1).setSeconds(0,0)).toISOString(),
-      end: new Date(item.data + hourIntervalStart).toISOString()
-    };
-
-    const secondPeriod = {
-      ...data,
-      start: new Date(item.data + hourIntervalEnd).toISOString(),
-      end: new Date(new Date(item.marcacao2).setSeconds(0,0)).toISOString()
-    };
-    
-    formattedList.push(firstPeriod, secondPeriod);
-  });
-
-  loginClockfy(formattedList, user);
-}
-
-function loginClockfy(formattedList, user) {
+function loginClockfy(user) {
   const data = JSON.stringify({
     "email": user.email,
     "password": user.password
@@ -111,7 +57,7 @@ function loginClockfy(formattedList, user) {
         memberShip: JSON.parse(this.responseText).membership[0].targetId,
         token: JSON.parse(this.responseText).token
       }
-      recordsOnMonth(login, formattedList);
+      findWorkspaceId(user, login);
     }
   });
   
@@ -121,7 +67,86 @@ function loginClockfy(formattedList, user) {
   xhr.send(data);
 }
 
-function recordsOnMonth(login, formattedList) {
+function findWorkspaceId(user, login) {
+  const term = user.prefix.substring(user.prefix.lastIndexOf("|") + 1).trim().toLowerCase();
+
+  const data = null;
+
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      if (!JSON.parse(this.responseText)[0]) {
+        alert('Projeto não encontrado, verifique o formato do prefixo informado!');
+        return;
+      }
+
+      workspaceId = JSON.parse(this.responseText)[0].id;
+      formatList(user, login);
+    }
+  });
+
+  xhr.open("GET", `https://global.api.clockify.me/workspaces/${login.memberShip}/projects/list?=&name=${term}`);
+  xhr.setRequestHeader("X-Auth-Token", login.token);
+  xhr.setRequestHeader("Authorization", "Bearer ");
+
+  xhr.send(data);
+}
+
+function formatList(user, login) {
+  const tasksToArray = user.activity.split("\n");
+
+  const dataTask = [];
+
+  tasksToArray.map(item => {
+    const task = {
+      date: item.trim().substring(0, item.trim().indexOf(" ")),
+      description: item.trim().substring(item.trim().indexOf(" ")).trim()
+    }
+    dataTask.push(task);
+  });
+
+  const DESCRIPTION_DEFAULT = user.prefix;
+  const hourIntervalStart = " " + user.start_interval + ":00";
+  const hourIntervalEnd = " " + user.end_interval + ":00";
+
+  formattedList = [];
+
+  list = list.filter(item => item.marcacao1 != null);
+  list = list.slice(0, 3);
+
+  list.map(item => {
+    const descriptionTask = dataTask.filter(task => task.date === item.dt);
+    const description = descriptionTask[0] ? DESCRIPTION_DEFAULT + " | " + (descriptionTask[0].description) : DESCRIPTION_DEFAULT;
+
+    const data = {
+      billable: true,
+      description: description,
+      projectId: workspaceId,
+      taskId: null,
+      tagIds: null
+    }
+
+    const firstPeriod = {
+      ...data,
+      start: new Date(new Date(item.marcacao1).setSeconds(0,0)).toISOString(),
+      end: new Date(item.data + hourIntervalStart).toISOString()
+    };
+
+    const secondPeriod = {
+      ...data,
+      start: new Date(item.data + hourIntervalEnd).toISOString(),
+      end: new Date(new Date(item.marcacao2).setSeconds(0,0)).toISOString()
+    };
+    
+    formattedList.push(firstPeriod, secondPeriod);
+  });
+
+  recordsOnMonth(formattedList, login);
+}
+
+function recordsOnMonth(formattedList, login) {
   const data = null;
 
   const xhr = new XMLHttpRequest();
@@ -131,7 +156,7 @@ function recordsOnMonth(login, formattedList) {
     if (this.readyState === this.DONE) {
       listRecordsOnMonth = JSON.parse(this.responseText).durationMap;
 
-      formattedList.map(item => insertRecordClockfy(login.token, item));
+      formattedList.map(item => insertRecordClockfy(item, login));
     }
   });
 
@@ -141,26 +166,7 @@ function recordsOnMonth(login, formattedList) {
   xhr.send(data);
 }
 
-function findWorkspaceId(token, term) {
-  const data = null;
-
-  const xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      workspaceId = JSON.parse(this.responseText).durationMap;
-    }
-  });
-
-  xhr.open("GET", `https://global.api.clockify.me/workspaces/5cd5b8daf15c98690baa2da3/projects/list?=&name=${term}`);
-  xhr.setRequestHeader("X-Auth-Token", token);
-  xhr.setRequestHeader("Authorization", "Bearer ");
-
-  xhr.send(data);
-}
-
-function insertRecordClockfy(token, item) {
+function insertRecordClockfy(item, login) {
   // Verifica se já existe registro neste dia no clockfy
   if (listRecordsOnMonth[item.start.substring(0, 10)]) return;
 
@@ -175,9 +181,9 @@ function insertRecordClockfy(token, item) {
     }
   });
   
-  xhr.open("POST", "https://global.api.clockify.me/workspaces/5cd5b8daf15c98690baa2da3/timeEntries/full");
+  xhr.open("POST", `https://global.api.clockify.me/workspaces/${login.memberShip}/timeEntries/full`);
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("X-Auth-Token", token);
+  xhr.setRequestHeader("X-Auth-Token", login.token);
   
   xhr.send(data);
 }
